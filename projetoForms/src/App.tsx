027ced3d404ed26eeb1e44ed2546cea00acf97ab
RodrigoMarquesPF <rodrigomarquespf@gmail.com>
2024-01-31 15:43:04 -0300

    front

--- projetoForms/src/App.tsx
+++ projetoForms/src/App.tsx
@@ -1,9 +1,99 @@
+import { Button } from "./components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./components/ui/dialog";
+import { Input } from "./components/ui/input";
+import { Label } from "./components/ui/label";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./components/ui/table";
+import { Search, PlusCircle } from "lucide-react";
+
 export function App() {
-
-
   return (
-    <h1>Helooooo</h1>
-  )
+    <div className="p-6 max-w-4xl mx-auto space-y-4">
+      <h1 className="text-4xl font-bold">Produots</h1>
+      <div className="flex items-centr justify-between">
+        <form className="flex items-center gap-2">
+          <Input name="id" placeholder="Id do pedido"></Input>
+          <Input name="name" placeholder="Nome do produto"></Input>
+          <Button type="submit" variant={"link"}>
+            <Search className="w-4 h-4 mr-2" />
+            Filtrar resultados
+          </Button>
+        </form>
+
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>
+              <PlusCircle className="w-4 h-4 mr-2" />
+              Novo Produto
+            </Button>
+          </DialogTrigger>
+
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Novo Produto</DialogTitle>
+              <DialogDescription>
+                Criar um novo produto no sistema
+              </DialogDescription>
+            </DialogHeader>
+            <form className="space-y-6">
+              <div className="grid grid-cols-4 items-center text-right gap-3">
+                <Label htmlFor="name">Produto</Label>
+                <Input className="col-span-3" id="name" />
+              </div>
+
+              <div className="grid grid-cols-4 items-center text-right gap-3">
+                <Label htmlFor="price">Preço</Label>
+                <Input className="col-span-3" id="price" />
+              </div>
+
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button type="button" variant={"outline"}>
+                    Cancelar
+                  </Button>
+                </DialogClose>
+                <Button type="submit">Salvar</Button>
+              </DialogFooter>
+            </form>
+          </DialogContent>
+        </Dialog>
+      </div>
+
+      <div className="border rounded-lg p-2">
+        <Table>
+          <TableHeader>
+            <TableHead>ID</TableHead>
+            <TableHead>Produto</TableHead>
+            <TableHead>Preço</TableHead>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: 10 }).map((_, i) => {
+              return (
+                <TableRow key={i}>
+                  <TableCell>ifodmf9e0fuf8</TableCell>
+                  <TableCell>Produto {i}</TableCell>
+                  <TableCell>R$ 122,00</TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
 }
-
-
